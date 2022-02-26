d4ccc38fb48ccd73a5ccf02e5a9a801883084334
adsw2022 <adsw2022@umoar.edu.sv>
2022-02-26 11:13:35 -0600

    Creacion de Componentes cabecero, ingreso, egreso y formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { EgresoComponent } from './egreso/egreso.component';
+import { CabeceroComponent } from './cabecero/cabecero.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngresoComponent,
+    EgresoComponent,
+    CabeceroComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule
