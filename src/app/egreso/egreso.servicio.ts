7517585dc4cf3a9c4dc341e748dbf9f03c0a48a0
dantrax <df.ortiz82@gmail.com>
2022-02-28 16:02:31 -0600

    Agregar Pipes de Moneda y Porcentaje

--- src/app/egreso/egreso.servicio.ts
+++ src/app/egreso/egreso.servicio.ts
@@ -3,7 +3,9 @@ import { Egreso } from './egreso.model';
 export class EgresoServicio {
   egresos: Egreso[] = [
     new Egreso('Alquiler Casa', 150),
-    new Egreso('Ropa', 125)
+    new Egreso('Ropa', 125),
+    new Egreso('Transporte', 140),
+    new Egreso('Supermercado', 350)
   ];
   eliminar(egreso:Egreso){
     const indice:number= this.egresos.indexOf(egreso);
