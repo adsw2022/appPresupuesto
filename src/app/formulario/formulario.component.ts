27c057aaede3fb6b8ebac18eba627ee328c4f576
dantrax <df.ortiz82@gmail.com>
2022-03-03 14:50:47 -0600

    agregando validacion al formulario

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Egreso } from '../egreso/egreso.model';
+import { EgresoServicio } from '../egreso/egreso.servicio';
+import { Ingreso } from '../ingreso/ingreso.model';
+import { IngresoServicio } from '../ingreso/ingreso.servicio';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
 })
 export class FormularioComponent implements OnInit {
+  tipo: string = 'Ingreso';
+  descripcionInput: string;
+  valorInput: number;
+  constructor(private ingresoService: IngresoServicio, private egresoService:EgresoServicio) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  tipoOperacion(evento) {
+    this.tipo = evento.target.value;
   }
-
+  agregarValor(){
+if(this.tipo==="Ingreso"){
+this.ingresoService.ingresos.push(new Ingreso(this.descripcionInput,this.valorInput));
+}
+else{
+  this.egresoService.egresos.push(new Egreso(this.descripcionInput,this.valorInput));
+}
+  }
 }
